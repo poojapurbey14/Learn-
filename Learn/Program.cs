6ebe892a5c67a07383cb537f43d1c338cdc1a30b
poojapurbey14 <59236192+poojapurbey14@users.noreply.github.com>
2020-08-31 09:09:59 -0400

    Add project files.

---
+++ Learn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Program program = new Program();
+            Console.WriteLine($"Sum of 2+3+4 is  :{program.MyMethod(2, 3, 4)}");
+        }
+        public int MyMethod(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+    }
+}
